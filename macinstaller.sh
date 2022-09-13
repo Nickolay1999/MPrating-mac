6d5545e95e836981145df065206091a8689742b3
Nickolay1999 <nikola.gundyrev@mail.ru>
2022-09-13 21:31:44 +0300

    init

---
+++ macinstaller.sh
@@ -0,0 +1,3 @@
+sudo xattr -cr *.jar
+sudo xattr -cr chromedrivers/chromedriver
+sudo xattr -cr 3proxy/bin/*
---
+++ runMe.command
@@ -0,0 +1,2 @@
+java -noverify -Dfile.encoding=UTF-8 -jar MPrating.jar 2> last.log?
+cat last.log?>>log.log
