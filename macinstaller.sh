8b30abeecab3c700f4d826fb2f0c83c317e35a3b
Nickolay1999 <nikola.gundyrev@mail.ru>
2023-10-12 10:46:02 +0300

    1.13.23

--- macinstaller.sh
+++ macinstaller.sh
@@ -1,3 +1,6 @@
 sudo xattr -cr *.jar
-sudo xattr -cr chromedrivers/chromedriver
+sudo xattr -cr chromedrivers/*
 sudo xattr -cr 3proxy/bin/*
+sudo chmod +x *.jar
+sudo chmod +x chromedrivers/*
+sudo chmod +x 3proxy/bin/*
